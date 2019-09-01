f2d9e585c70e5454d3d270f8a27b10af60e758ea
Zeeshanfazal91 <54771124+Zeeshanfazal91@users.noreply.github.com>
2019-09-01 18:55:18 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="CustomCss/homepage.css">
+
+<style type="text/css">
+  .rightRound
+  {
+    border-radius: 0px 50px 50px 0px;
+    background-color: rgb(42, 133, 63);
+    border-width: 1px;
+    width: 110px;
+   
+  }
+
+  .leftRound
+  {
+    border-radius: 50px 0px 0px 50px;
+    border-width: 2px;
+    border-color: rgb(42, 133, 63);
+
+  }
+
+  @media only screen and (min-width: 1100px) 
+  {
+  .leftRound{
+    width: 800px !important;
+  }
+}
+</style>
+
+    <title>Pak Deals Pro</title>
+  </head>
+  <body>
+   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a style="margin-left: 20px;" class="navbar-brand mb-0 h1" href="#">PakDealsPro</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+
+     <div style="margin-top: 15px; margin-left: 20px;" class="container">
+   
+    <div class="input-group">
+        <input class="form-control border-right-0 leftRound">
+        <span class="input-group-addon bg-white border-left-0"><i class="ion-search"></i></span>
+        <button class="rightRound" style="color: white;">Search</button>
+    </div>
+
+    </div>
+    </ul>
+   
+  </div>
+</nav>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
